62231a5cc14e1a95b6e6348a2a521a7f8ac358ca
dana <danaputra1001@gmail.com>
2025-10-11 09:48:20 +0700

    update register seller + fix kesalhan ketik di properti json

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 // deklarasi routes
-const { register, login, verifyOtp } = require('../controllers/authController');
+const { register, login, verifyOtp, registerSeller } = require('../controllers/authController');
 
 
 //router nya
 router.post('/register', register); // register users
 router.post('/login', login); //login usres
 router.post('/verify-otp', verifyOtp); //verify otp users
+router.post('/register-seller', registerSeller); //register seller
 
 module.exports = router;
