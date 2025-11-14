84c70ee393350d6abf6327a31833ffa179d33a65
danaputra133 <danaputra375@gmail.com>
2025-11-14 21:29:53 +0700

    update bagian seller tahap 1

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {
-  createProduct,
-  getSellerProducts,
-  updateProduct,
-  deleteProduct
-} = require('../controllers/productController');
-
+  getSellerProfile,
+  updateSellerProfile, changeSellerPassword,
+} = require("../controllers/sellerController");
 const { protect, authorize } = require('../middleware/protectMiddleware');
 const authBackend = require('../middleware/authBackend');
 
 // Semua route product lewat verifikasi backend + JWT seller
 router.use(authBackend);
 
-router.post('/', protect, authorize('seller'), createProduct);
-router.get('/', protect, authorize('seller'), getSellerProducts);
-router.put('/:id', protect, authorize('seller'), updateProduct);
-router.delete('/:id', protect, authorize('seller'), deleteProduct);
+
+router.get('/profile', protect, authorize('seller'), getSellerProfile);
+router.put("/profile", protect, authorize("seller"), updateSellerProfile);
+router.put('/change-password-seller', protect, authorize('seller'), changeSellerPassword);
 
 module.exports = router;
